2bc26f0893eb851a02be25f80f0ded5cffe91a81
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2024-03-13 10:04:32 +0530

    #include<stdio.h> int square(int n) {     int i=1;     while(i<(n/3))     {         if(i*i==n)         {             return 1;         }         i++;     }     return 0; } int main() {     int n,i=1;     scanf("%d",&n);     if(square(n)==1)     {         printf("True");     }     else     {         printf("False");     } }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
+int square(int n)
+{
+    int i=1;
+    while(i<(n/3))
+    {
+        if(i*i==n)
+        {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
 int main()
 {
-    int n,i;
+    int n,i=1;
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
+    if(square(n)==1)
     {
-        if(n==i*i)
-        {
-            printf("True");
-            return 0;
-        }
+        printf("True");
     }
-    printf("False");
-    return 0;
+    else
+    {
+        printf("False");
+    }
 }
